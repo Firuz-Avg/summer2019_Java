2cf995e31ac88a5200e8c82ebd97ac87a987eb4c
Firuz-Avg <firuz.avg@gmail.com>
2019-09-30 13:35:56 -0500

    Todays topic

---
+++ Summer 2019_Java/src/day29_ReturnMethods/MethodDecleration.java
@@ -0,0 +1,20 @@
+package day29_ReturnMethods;
+
+public class MethodDecleration {
+	/*
+	method decleration: 
+	    access-modifier specifier  returnType name(parameter){}
+	         public       static     void 
+	    return-Type: void or other data-types (int, String..)
+	            if the return is not void, it MUST be primitive or non primitives.
+	    if the return-type is void, the method does not return any value
+	    if the retun-Type is not void: the method MUST return a value
+	return statement: it's used for exiting the method, we can also use return statement to retun a vlue from the method
+	return method:
+	            1. does not have "void" as return Type
+	            2. MUSt return a Value
+	            3. the returning value MUST macth with the return-Type of the method
+	            4. the method body Must be closed with return statement
+	 */
+
+}
