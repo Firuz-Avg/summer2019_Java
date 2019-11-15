2bf811ad5d6f7fe63dc095c461d9153ba3eade93
Firuz-Avg <firuz.avg@gmail.com>
2019-11-15 17:59:32 -0600

    ?

--- Summer 2019_Java/src/Trainings/whatBurger.java
+++ Summer 2019_Java/src/Trainings/whatBurger.java
@@ -3,7 +3,7 @@ package Trainings;
 public class whatBurger {
 	
    public static void main(String[] args) {
-	Integer myInt = 10011;
+	Integer myInt = 100;
 	String myString = myInt.toString();
 	System.out.println(myString.length());
 }
--- Summer 2019_Java/src/day52_Interface/InterfaceIntro.java
+++ Summer 2019_Java/src/day52_Interface/InterfaceIntro.java
@@ -1,5 +1,35 @@
 package day52_Interface;
 
-public class InterfaceIntro {
+public interface InterfaceIntro {
+	
+//	public InterfaceIntro() { } interface cannot have constructor
+	
+//	public void methodB() { }
+	
+	public default void methodC() {
+		
+	}
+	
+	public abstract void methodA();
+	
+	public static void mm() {
+		
+	}
+	
+	
 
+}
+
+interface Data{
+	
+}
+
+class Test implements InterfaceIntro, Data{
+//    subtype        supertype      supertype
+	
+	@Override
+	public void methodA() {
+		
+	}
+	
 }
